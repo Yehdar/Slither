6e2b889cdc964754e426f4dd4214fa841a1602c6
Yehdar <p.radhey06@gmail.com>
2023-07-03 15:53:42 -0400

    import required structs to parse html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate html5ever;
 extern crate url;
 
+mod parse;
+
 fn main() {
     println!("hollup... let him cook");
 }
---
+++ src/parse.rs
@@ -0,0 +1,6 @@
+use std::string::String;
+
+use html5ever::tendril::TendrilSink;
+use html5ever::parse_document;
+use html5ever::rcdom::{Handle, NodeData, RcDom};
+use html5ever::interface::Attribute;
