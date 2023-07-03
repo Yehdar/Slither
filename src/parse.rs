a82c348a2956beb8c07e76db516c1c47214a41d4
Yehdar <p.radhey06@gmail.com>
2023-07-03 16:02:09 -0400

    parse document and return RcDOM

--- src/parse.rs
+++ src/parse.rs
@@ -4,3 +4,9 @@ use html5ever::tendril::TendrilSink;
 use html5ever::parse_document;
 use html5ever::rcdom::{Handle, NodeData, RcDom};
 use html5ever::interface::Attribute;
+
+fn parse_html(source: &str) -> RcDom {
+    parse_document(RcDom::default(), Default::default())
+        .from_utf8()
+        .read_from(&mut source.as_bytes())
+}
