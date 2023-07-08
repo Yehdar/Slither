bfd9e9ec019c1cffa5c98bce0ea23fb8f0e026cd
Yehdar <p.radhey06@gmail.com>
2023-07-08 08:15:48 -0400

    store url handles in a vector

--- src/parse.rs
+++ src/parse.rs
@@ -5,8 +5,14 @@ use html5ever::parse_document;
 use html5ever::rcdom::{Handle, NodeData, RcDom};
 use html5ever::interface::Attribute;
 
-fn parse_html(source: &str) -> RcDom {
+pub fn parse_html(source: &str) -> RcDom {
     parse_document(RcDom::default(), Default::default())
         .from_utf8()
         .read_from(&mut source.as_bytes())
+        .unwrap()
+}
+
+pub fn get_urls(handle: Handle) -> Vec<String> {
+    let mut urls = vec![];
+    let mut anchor_tags = vec![];
 }
