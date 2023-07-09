7a9caded9191e1b5a8b652c95359da6027347c17
Yehdar <p.radhey06@gmail.com>
2023-07-09 15:48:31 -0400

    build: add external crates, packages and self receivers

---
+++ src/fetch.rs
@@ -0,0 +1,12 @@
+extern crate hyper;
+extern crate url;
+
+use std::io::Read;
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc::channel;
+use std::fmt;
+
+use self::hyper::Client;
+use self::hyper::status::StatusCode;
+use self::url::{ParseResult, Url, UrlParser};
