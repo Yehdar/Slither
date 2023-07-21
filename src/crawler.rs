f42013a5ddbaa816500a58739f154d1c028476e9
Yehdar <p.radhey06@gmail.com>
2023-07-21 10:16:06 -0400

    chore: remove git comments

--- src/crawler.rs
+++ src/crawler.rs
@@ -58,7 +58,6 @@ fn crawl_worker_thread(
             current = to_visit_val.pop().unwrap();
             *active_count_val += 1;
             assert!(*active_count_val <= THREADS);
-<<<<<<< HEAD
         }
 
         {
@@ -70,8 +69,6 @@ fn crawl_worker_thread(
             } else {
                 visited_val.insert(current.to_owned());
             }
-=======
->>>>>>> 00c8e8d789327e4b80d0fb0756936c44aa1c75b4
         }
     }
 }
