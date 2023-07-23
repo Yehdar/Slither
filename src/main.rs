4ea435c21f32dd2c73e86e6394d37660b23b1785
Yehdar <p.radhey06@gmail.com>
2023-07-23 19:24:17 -0400

    implement web crawler in main function
    hardcoded but it works. Anyways, the provided command-line argument is parsed and the domain is extracted. Added counter variables to track number of successful and failed URL accesses. The web cralwer is invoked using the extracted domain, which results in it iterating through URL states. Obviously, added handling for the URL state to increment counters. Finally, a progress display at the very end

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ extern crate html5ever;
 extern crate url;
 
 use std::env;
+use std::io::stdout;
+use std::io::Write;
+use url::Url;
+
+use fetch::UrlState;
 
 mod parse;
 mod fetch;
@@ -11,5 +16,25 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() > 1 {
         let start_url_string = &args[1];
+
+        let start_url = Url::parse(start_url_string).unwrap();
+        let domain = start_url.domain().expect("can't find a domain in your URL");
+
+        let mut success_counter = 0;
+        let mut fail_counter = 0;
+
+        for url_state in crawler::crawl(&domain, &start_url) {
+            match url_state {
+                UrlState::Accessible(_) => {
+                    success_counter += 1;
+                }
+                status => {
+                    fail_counter += 1;
+                    println!("{}", status);
+                }
+            }
+            print!("Succeeded: {} Failed: {}\r", success_counter, fail_counter);
+            stdout().flush().unwrap();
+        }
     }
 }
